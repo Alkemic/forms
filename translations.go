53a75beb05902eba64701e576ff36eb3b65aba81
Daniel Alkemic Czuba <alkemic7@gmail.com>
2018-06-18 15:03:57 +0200

    Translation map directly as a variable

--- translations.go
+++ translations.go
@@ -1,19 +1,15 @@
 package forms
 
-var translations map[string]string
+var translations map[string]string = map[string]string{
+	"REQUIRED": "This field can't be empty",
 
-func init() {
-	translations = map[string]string{
-		"REQUIRED": "This field can't be empty",
+	"INCORRECT_EMAIL": "\"%s\" is not correct email address",
 
-		"INCORRECT_EMAIL": "\"%s\" is not correct email address",
+	"INCORRECT_MULTI_VAL":  "You supplied more than one value for this field",
+	"INCORRECT_MIN_LENGTH": "Value \"%%s\" need to be at least %d chars long",
+	"INCORRECT_MAX_LENGTH": "Value \"%%s\" need to be at max %d chars long",
 
-		"INCORRECT_MULTI_VAL":  "You supplied more than one value for this field",
-		"INCORRECT_MIN_LENGTH": "Value \"%%s\" need to be at least %d chars long",
-		"INCORRECT_MAX_LENGTH": "Value \"%%s\" need to be at max %d chars long",
+	"NO_MATCH_PATTERN": "Value \"%%s\" doesn't match pattern \"%s\"",
 
-		"NO_MATCH_PATTERN": "Value \"%%s\" doesn't match pattern \"%s\"",
-
-		"VALUE_NOT_FOUND": "Value \"%s\" not found in slice",
-	}
+	"VALUE_NOT_FOUND": "Value \"%s\" not found in slice",
 }
